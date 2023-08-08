7db36c12134b549821338c2eedae34229452688a
qudus <qudus@ecscorpresources.com>
2023-08-08 18:16:57 +0100

    feat: get prroducts data

---
+++ components/Categories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="w-[30%] bg-green-200">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae molestias
+      sunt deleniti ab eius? Illo dolores voluptates itaque nemo et, vel, dolor
+      perferendis non quasi sequi quibusdam ad quisquam aperiam!
+    </div>
+  );
+};
+
+export default Categories;
